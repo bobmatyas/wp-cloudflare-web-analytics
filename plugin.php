62210631c256a3aec4398ba9f2502b0f8e61103f
bob <45246438+bobmatyas@users.noreply.github.com>
2022-11-27 13:51:41 -0500

    fix phpcs issues

--- plugin.php
+++ plugin.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * This plugin adds support for Cloudflare Web Analytics in WordPress.
+ *
+ * @package "Cloudflare Web Analytics"
+ * @version 1.0.0
+ *
+ * @wordpress-plugin
  * Plugin Name:       Cloudflare Web Analytics
  * Plugin URI:        https://www.bobmatyas.com
  * Description:       Easily add Cloudflare Web Analytics to WordPress
@@ -14,8 +20,7 @@
  * Domain Path:       /public/lang
  */
 
- /*
-
+/*
 Copyright (C) 2022  Bob Matyas
 
 This program is free software; you can redistribute it and/or modify
@@ -38,121 +43,126 @@ add_action( 'admin_menu', 'cf_web_analytics_add_settings_menu' );
 
 function cf_web_analytics_add_settings_menu() {
 
-    add_options_page( 'Cloudflare Web Analytics Settings', 'Cloudflare Web Analytics', 'manage_options',
-        'cf_web_analytics', 'cf_web_analytics_option_page' );
+	add_options_page(
+		'Cloudflare Web Analytics Settings',
+		'Cloudflare Web Analytics',
+		'manage_options',
+		'cf_web_analytics',
+		'cf_web_analytics_option_page'
+	);
 
 }
 
 function cf_web_analytics_option_page() {
-    ?>
-    <div class="wrap">
-        <h2>Cloudflare Web Analytics</h2>
-        <form action="options.php" method="post">
-            <?php
-            settings_fields( 'cf_web_analytics_options' );
-            do_settings_sections( 'cf_web_analytics' );
-            submit_button( 'Save', 'primary' );
-            ?>
-        </form>
-    </div>
-    <?php
+	?>
+	<div class="wrap">
+		<h2>Cloudflare Web Analytics</h2>
+		<form action="options.php" method="post">
+			<?php
+			settings_fields( 'cf_web_analytics_options' );
+			do_settings_sections( 'cf_web_analytics' );
+			submit_button( 'Save', 'primary' );
+			?>
+		</form>
+	</div>
+	<?php
 }
 
-add_action('admin_init', 'cf_web_analytics_admin_init');
+add_action( 'admin_init', 'cf_web_analytics_admin_init' );
 
+function cf_web_analytics_admin_init() {
 
-function cf_web_analytics_admin_init(){
+	$args = array(
+		'type'              => 'string',
+		'sanitize_callback' => 'cf_web_analytics_validate_options',
+		'default'           => null,
+	);
 
-    $args = array( 
-        'type'              => 'string',
-        'sanitize_callback' => 'cf_web_analytics_validate_options',
-        'default'           => NULL
-    );
+	register_setting( 'cf_web_analytics_options', 'cf_web_analytics_options', $args );
 
-    register_setting( 'cf_web_analytics_options', 'cf_web_analytics_options', $args );
+	add_settings_section(
+		'cf_web_analytics_main',
+		'Cloudflare Web Analytics Settings',
+		'cf_web_analytics_section_text',
+		'cf_web_analytics'
+	);
 
-    add_settings_section(
-        'cf_web_analytics_main',
-        'Cloudflare Web Analytics Settings',
-        'cf_web_analytics_section_text',
-        'cf_web_analytics'
-    );
-
-    add_settings_field( 
-        'cf_web_analytics_token',
-        'Token',
-        'cf_web_analytics_setting_token',
-        'cf_web_analytics',
-        'cf_web_analytics_main'
-    );
+	add_settings_field(
+		'cf_web_analytics_token',
+		'Token',
+		'cf_web_analytics_setting_token',
+		'cf_web_analytics',
+		'cf_web_analytics_main'
+	);
 }
 
 function cf_web_analytics_section_text() {
-    
-    echo '<p>Enter your token. Add instructions here.</p>';
+
+	echo '<p>Enter your token. Add instructions here.</p>';
 
 }
 
 function cf_web_analytics_setting_token() {
 
-    $options = get_option( 'cf_web_analytics_options' );
-    $token = $options['token'];
+	$token = cf_web_analytics_get_token();
 
-    echo "<input id='token' name='cf_web_analytics_options[token]' pattern='[a-zA-Z0-9-]+' type='text' value='" . esc_attr( $token ) . "'/>";
+	echo "<input id='token' name='cf_web_analytics_options[token]' pattern='[a-zA-Z0-9-]+' type='text' value='" . esc_attr( $token ) . "'/>";
 
 }
 
 function cf_web_analytics_validate_options( $input ) {
 
-    $valid = array();
-    $valid['token'] = preg_replace(
-        '/[^A-Za-z0-9]/',
-        '',
-        $input['token'] );
+	$valid          = array();
+	$valid['token'] = preg_replace(
+		'/[^A-Za-z0-9]/',
+		'',
+		$input['token']
+	);
 
-    if( $valid['token'] !== $input['token'] ) {
+	if ( $valid['token'] !== $input['token'] ) {
 
-        add_settings_error(
-            'cf_web_analytics_text_string',
-            'cf_web_analytics_texterror',
-            'Incorrect value entered. Token should be only letters and numbers.',
-            'error'
-        );
+		add_settings_error(
+			'cf_web_analytics_text_string',
+			'cf_web_analytics_texterror',
+			'Incorrect value entered. Token should be only letters and numbers.',
+			'error'
+		);
 
-    }
+	}
 
-    $valid['token'] = sanitize_text_field( $input['token'] );
+	$valid['token'] = sanitize_text_field( $input['token'] );
 
-    return $valid;
+	return $valid;
 
 }
 
-function cf_web_analytics_getToken() {
+function cf_web_analytics_get_token() {
 
-    $options = get_option( 'cf_web_analytics_options' );
-    $token = $options['token'];
+	$options = get_option( 'cf_web_analytics_options' );
+	$token   = $options['token'];
 
-    return $token;
+	return $token;
+
 }
 
 function cf_web_analytics_load_scripts() {
 
-    $token = cf_web_analytics_getToken();
+	$token = cf_web_analytics_get_token();
 
-    print 'token: '. $token .'';
+	if ( '' === $token || null === $token ) {
 
-    if ( '' == $token || null == $token ) {
-        return;
-    }
-    
-    wp_enqueue_script(
-        'cf-web-analytics',
-        'https://static.cloudflareinsights.com/beacon.min.js',
-        null,
-        null,
-        true
-    );
-    
+		return;
+
+	}
+
+	wp_enqueue_script(
+		'cf-web-analytics',
+		'https://static.cloudflareinsights.com/beacon.min.js',
+		null,
+		'1.0',
+		true
+	);
+
 }
 
 add_action( 'wp_enqueue_scripts', 'cf_web_analytics_load_scripts' );
@@ -160,13 +170,13 @@ add_action( 'wp_enqueue_scripts', 'cf_web_analytics_load_scripts' );
 
 function cf_web_analytics_add_attributes( $tag, $handle, $src ) {
 
-    $token = cf_web_analytics_getToken();
+	$token = cf_web_analytics_get_token();
 
-    if ( 'cf-web-analytics' === $handle ) {
-        $tag = "<script defer src='" . esc_url( $src ) . "'  data-cf-beacon='{\"token\": \"". $token ."\"}'></script>";
-    }
+	if ( 'cf-web-analytics' === $handle ) {
+		$tag = "<script defer src='" . esc_url( $src ) . "'  data-cf-beacon='{\"token\": \"". $token ."\"}'></script>";
+	}
 
-    return $tag;
+	return $tag;
 }
 
 add_filter( 'script_loader_tag', 'cf_web_analytics_add_attributes', 10, 3 );
