a68ddd25fa8c7c27ca4df3b9924952034f60b32d
bob <45246438+bobmatyas@users.noreply.github.com>
2026-07-04 21:41:52 -0400

    Update text domain string

--- plugin.php
+++ plugin.php
@@ -16,7 +16,7 @@
  * Author URI:        https://www.bobmatyas.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       cf-web-analytics
+ * Text Domain:       cloudflare-web-analytics
  */
 
 /*
