561ae1a512c3954879cdc5e4037e6f95ffd797d4
bob <45246438+bobmatyas@users.noreply.github.com>
2026-07-04 21:45:22 -0400

    Add resource guard for PHP files

--- plugin.php
+++ plugin.php
@@ -37,5 +37,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 require __DIR__ . '/includes/settings.php';
 require __DIR__ . '/includes/load-script.php';
